a632b34190f83259a6df7aead7740a3ee6d0891c
chiragpatel1499 <chiragpatel1499@gmail.com>
2020-11-25 16:48:28 +0530

    message

--- helloworld/functions/index.js
+++ helloworld/functions/index.js
@@ -1,9 +1,9 @@
 const functions = require('firebase-functions');
-//hello hii
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloworld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs! changes", {structuredData: true});
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
    response.send("Hello from Firebase!");
-});
+ });
